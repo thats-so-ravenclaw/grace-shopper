535225ae07d75205927b484e12fe24a07cf96fe1
Lyndsey Reese <lmariereese@gmail.com>
2019-09-18 12:45:09 -0400

    Feature: all wigs component

--- client/components/all-wigs.js
+++ client/components/all-wigs.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import getAllWigs from './store/wigs';
 
 class AllWigs extends React.Component {
+  componentDidMount() {
+    this.props.getAllWigs();
+  }
+
   render() {
+    const wigs = this.props.wigs;
     return (
       <div>
         <h1>Shop our wigs!</h1>
         <div className="container wigs">
-          <div className="wig-card">
-            <img src="" />
-            <h3>Wig name</h3>
-            <p>Price</p>
-            <button type="button">View</button>
-          </div>
+          {wigs === undefined
+            ? ''
+            : wigs.map(wig => (
+                <div className="wig-card" key={wig.id}>
+                  <img src={wig.image} />
+                  <h3>{wig.name}</h3>
+                  <p>{wig.price}</p>
+                  <button type="button">View</button>
+                </div>
+              ))}
         </div>
       </div>
     );
@@ -23,6 +33,8 @@ const mapStateToProps = state => ({
   wigs: state.wigs
 });
 
-// const mapDispatchToProps = (dispatch) => ({
+const mapDispatchToProps = dispatch => ({
+  getWigs: () => dispatch(getAllWigs())
+});
 
-// });
+export default connect(mapStateToProps, mapDispatchToProps)(AllWigs);
