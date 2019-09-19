c57b3b1573ed8af022c7fa2a40793bfaad39969d
Nevette Bailey <nevettebailey@gmail.com>
2019-09-19 13:00:50 -0400

    Feature: Checkout Form

--- client/components/checkoutForm.js
+++ client/components/checkoutForm.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { connect } from 'react-redux';
+//need to import thunks to post an order
 
 class CheckoutForm extends React.Component {
   constructor() {
     super();
     this.state = {
-      items: [],
       name: '',
       shippingAddressStreet: '',
       shippingAddressCity: '',
@@ -17,6 +17,7 @@ class CheckoutForm extends React.Component {
       billingAddressZipcode: ''
     };
     this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
   handleChange(event) {
     this.setState({
@@ -47,8 +48,101 @@ class CheckoutForm extends React.Component {
               onChange={this.handleChange}
             />
           </div>
+          <div>
+            <h4>Shipping Address:</h4>
+            <div>
+              <label htmlFor="shippingAddressStreet">Street:</label>
+              <input
+                type="text"
+                name="shippingAddressStreet"
+                value={this.state.shippingAddressStreet}
+                onChange={this.handleChange}
+              />
+            </div>
+            <div>
+              <label htmlFor="shippingAddressCity">City:</label>
+              <input
+                type="text"
+                name="shippingAddressCity"
+                value={this.state.shippingAddressCity}
+                onChange={this.handleChange}
+              />
+            </div>
+            <div>
+              <label htmlFor="shippingAddressState">State:</label>
+              <input
+                type="text"
+                name="shippingAddressState"
+                value={this.state.shippingAddressState}
+                onChange={this.handleChange}
+              />
+            </div>
+            <div>
+              <label htmlFor="shippingAddressZipcode">Zip:</label>
+              <input
+                type="text"
+                name="shippingAddressZipcode"
+                value={this.state.shippingAddressZipcode}
+                onChange={this.handleChange}
+              />
+            </div>
+          </div>
+          <div>
+            <h4>Billing Address:</h4>
+            <div>
+              <label htmlFor="billingAddressStreet">Street:</label>
+              <input
+                type="text"
+                name="billingAddressStreet"
+                value={this.state.billingAddressStreet}
+                onChange={this.handleChange}
+              />
+            </div>
+            <div>
+              <label htmlFor="billingAddressCity">City:</label>
+              <input
+                type="text"
+                name="billingAddressCity"
+                value={this.state.billingAddressCity}
+                onChange={this.handleChange}
+              />
+            </div>
+            <div>
+              <label htmlFor="billingAddressState">State:</label>
+              <input
+                type="text"
+                name="billingAddressState"
+                value={this.state.billingAddressState}
+                onChange={this.handleChange}
+              />
+            </div>
+            <div>
+              <label htmlFor="billingAddressZipcode">Zip:</label>
+              <input
+                type="text"
+                name="billingAddressZipcode"
+                value={this.state.billingAddressZipcode}
+                onChange={this.handleChange}
+              />
+            </div>
+          </div>
+          <button type="submit">Place Order</button>
         </div>
       </form>
     );
   }
 }
+
+const mapStateToProps = state => {
+  return {
+    cart: state.cart
+  };
+};
+
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     //some thunks go here
+//   }
+// }
+
+export default connect(mapStateToProps)(CheckoutForm);
