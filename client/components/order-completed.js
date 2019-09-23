a08823fa2bdfaa1eca6e9cf7bd03459c512aa952
Lyndsey Reese <lmariereese@gmail.com>
2019-09-23 15:46:11 -0400

    Cleanup: removed commented out code

--- client/components/order-completed.js
+++ client/components/order-completed.js
@@ -1,16 +1,4 @@
 import React from 'react';
-import { connect } from 'react-redux';
-
-// class OrderCompleted extends React.component {
-
-//   render () {
-//     return (
-//       <div>
-//         <h3>Thank you for your purchase!</h3>
-//     </div>
-//     )
-//   }
-// }
 
 const OrderCompleted = () => {
   return (
