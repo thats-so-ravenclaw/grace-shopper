87c984d90ca27669b99351e47866feb4bb7a8549
Nevette Bailey <nevettebailey@gmail.com>
2019-09-24 11:54:20 -0400

    Fix: backed security for API routes

--- server/api/orders.js
+++ server/api/orders.js
@@ -4,22 +4,27 @@ module.exports = router;
 
 router.post('/', async (req, res, next) => {
   try {
-    const oneOrder = await Order.create({
-      status: 'submitted',
-      total: req.body.total,
-      name: req.body.name,
-      street: req.body.street,
-      city: req.body.city,
-      state: req.body.state,
-      zip: req.body.zip
-    });
+    //if the user is undefined or the user id equals the request user id then this ok, otherwise, redirect to login
+    if (req.user === undefined || req.body.user.id === req.user.id) {
+      const oneOrder = await Order.create({
+        status: 'submitted',
+        total: req.body.total,
+        name: req.body.name,
+        street: req.body.street,
+        city: req.body.city,
+        state: req.body.state,
+        zip: req.body.zip
+      });
 
-    if (oneOrder) {
-      // req.body.user will either be null (if a guest) or it'll be the user id (if user is logged in)
-      oneOrder.setUser(req.body.user);
-      res.status(201).send('Order successfully created.');
+      if (oneOrder) {
+        // req.body.user will either be null (if a guest) or it'll be the user id (if user is logged in)
+        oneOrder.setUser(req.body.user);
+        res.status(201).send('Order successfully created.');
+      } else {
+        res.status(400).send('Order not created.');
+      }
     } else {
-      res.status(400).send('Order not created.');
+      res.redirect('/login');
     }
   } catch (err) {
     next(err);
